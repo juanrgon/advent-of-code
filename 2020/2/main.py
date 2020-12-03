6eefb16308c7b00bcf9e6c75c7d4e6385b955fc3
Juan Gonzalez <jrg2156@gmail.com>
2020-12-03 00:41:00 -0500

    tidying up

--- 2020/2/main.py
+++ 2020/2/main.py
@@ -1,5 +1,6 @@
 import os.path
-from collections import defaultdict
+import re
+from collections import defaultdict, Counter
 
 
 def main():
@@ -10,34 +11,33 @@ def main():
 def _part_1():
     valid = 0
     for entry in _get_input():
-        policy, password = entry.split(":")
-        password = password.strip()
-        _range, letter = policy.split()
-        a, b = _range.split("-")
-        count = 0
-        for l in password:
-            if l == letter:
-                count += 1
-        if int(a) <= count <= int(b):
+        a, b, letter, password = _multisplit(entry, '-', ': ', ' ')
+
+        counts = Counter(password)
+
+        if int(a) <= counts[letter] <= int(b):
             valid += 1
+
     return valid
 
 
 def _part_2():
     valid = 0
     for entry in _get_input():
-        policy, password = entry.split(":")
-        password = password.strip()
-        _range, letter = policy.split()
-        a, b = _range.split("-")
+        a, b, letter, password = _multisplit(entry, '-', ': ', ' ')
+
         if (password[int(a) - 1] == letter) ^ (password[int(b) - 1] == letter):
             valid += 1
+
     return valid
 
 
 def _get_input():
     with open(os.path.join(os.path.abspath("."), "input")) as f:
         return f.read().splitlines()
+
+def _multisplit(string, *delimiters):
+    return re.split("|".join([re.escape(d) for d in delimiters]), string)
 
 
 if __name__ == "__main__":
