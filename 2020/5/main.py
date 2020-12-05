ab1dce9e8ec2a86307e9c49bec50e6027d248706
Juan Gonzalez <jrg2156@gmail.com>
2020-12-05 13:50:06 -0500

    Solution to 2020 Day 5

---
+++ 2020/5/main.py
@@ -0,0 +1,58 @@
+from collections import Counter, defaultdict
+import re
+from pathlib import Path
+import requests
+from itertools import cycle
+import __main__
+import os.path
+import attr
+
+
+@attr.s(auto_attribs=True)
+class A:
+    a: int
+
+
+def main():
+    print("Solution to Part 1: " + _part_1())
+    print("Solution to Part 2: " + _part_2())
+
+
+def _part_1():
+    highest_seat = float('-inf')
+    for seat in _input().split():
+        row = int(''.join(['1' if p == 'B' else '0' for p in seat[:-3]]), 2)
+        col = int(''.join(['1' if p == 'R' else '0' for p in seat[-3:]]), 2)
+        highest_seat = max(highest_seat, row * 8 + col)
+    return str(highest_seat)
+
+
+def _part_2():
+    h = float('-inf')
+    seats = []
+    for seat in _input().split():
+        row = int(''.join(['1' if p == 'B' else '0' for p in seat[:-3]]), 2)
+        col = int(''.join(['1' if p == 'R' else '0' for p in seat[-3:]]), 2)
+        seats.append(row * 8 + col)
+
+    previous = None
+    for seat in sorted(seats):
+        if previous is None:
+            previous = seat
+            continue
+
+        if seat == previous + 2:
+            return str(seat - 1)
+        previous = seat
+
+    return str(h)
+
+
+
+def _input():
+    with open(os.path.join(os.path.dirname(__main__.__file__),  'input')) as f:
+        return f.read()
+
+
+if __name__ == "__main__":
+    main()
