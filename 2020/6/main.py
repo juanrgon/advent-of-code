50d52ba5d4b379bed3ed8cfba3788d5a425bae9c
Juan Gonzalez <jrg2156@gmail.com>
2020-12-06 00:20:29 -0500

    Solution to 2020 Day 6

--- 2020/6/main.py
+++ 2020/6/main.py
@@ -1,11 +1,6 @@
 import os.path
-from collections import Counter, defaultdict
-import re
-from pathlib import Path
-import requests
-from itertools import cycle
+from collections import Counter
 import __main__
-
 
 
 def main():
@@ -17,9 +12,9 @@ def _part_1():
     _input()
 
     total = 0
-    for g in _input().split('\n\n'):
+    for g in _input().split("\n\n"):
         yes = set()
-        for question in g.replace("\n", ''):
+        for question in g.replace("\n", ""):
             yes.add(question)
         total += len(yes)
     return str(total)
@@ -29,9 +24,9 @@ def _part_2():
     _input()
 
     total = 0
-    for group in _input().split('\n\n'):
+    for group in _input().split("\n\n"):
         all_yes = set()
-        for question, count in Counter(group.replace('\n', '')).items():
+        for question, count in Counter(group.replace("\n", "")).items():
             if count == len(group.split()):
                 all_yes.add(question)
         total += len(all_yes)
@@ -39,9 +34,8 @@ def _part_2():
 
 
 def _input():
-    with open(os.path.join(os.path.dirname(__main__.__file__),  'input')) as f:
+    with open(os.path.join(os.path.dirname(__main__.__file__), "input")) as f:
         return f.read()
-
 
 
 if __name__ == "__main__":
