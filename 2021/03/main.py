e2e01b927d11b3d134387b7793578b3ab471f83a
Juan Gonzalez <jrg2156@gmail.com>
2021-12-03 09:43:09 -0500

    2021 Day 3

--- 2021/03/main.py
+++ 2021/03/main.py
@@ -1,4 +1,5 @@
-TEST ="""
+TEST = (
+    """
 00100
 11110
 10110
@@ -11,9 +12,12 @@ TEST ="""
 11001
 00010
 01010
-""", 198
+""",
+    198,
+)
 
-TEST2 ="""
+TEST2 = (
+    """
 00100
 11110
 10110
@@ -26,7 +30,9 @@ TEST2 ="""
 11001
 00010
 01010
-""", 230
+""",
+    230,
+)
 
 
 import sys
@@ -43,12 +49,45 @@ import aoc
 @aoc.tests([TEST])
 @aoc.parse_text
 def part_1(raw: str, ints: List[int], strs: List[str]):
-    return 0
+
+    bit_counts = defaultdict(lambda: {"0": 0, "1": 1})
+
+    for line in strs:
+        for i, bit in enumerate(line):
+            bit_counts[i][bit] += 1
+
+    gr = ""
+    er = ""
+    for counts in bit_counts.values():
+        gr += "1" if counts["1"] > counts["0"] else "0"
+        er += "1" if counts["1"] < counts["0"] else "0"
+
+    return int(gr, 2) * int(er, 2)
+
 
 @aoc.tests([TEST2])
 @aoc.parse_text
 def part_2(raw: str, ints: List[int], strs: List[str]):
-    return 0
+    oxy = strs
+    co2 = strs
+
+    for bit in range(len(strs[0])):
+        o = defaultdict(list)
+        c = defaultdict(list)
+
+        for n in oxy:
+            o[n[bit]].append(n)
+
+        for n in co2:
+            c[n[bit]].append(n)
+
+        if len(oxy) > 1:
+            oxy = o["1"] if len(o["1"]) >= len(o["0"]) else o["0"]
+
+        if len(co2) > 1:
+            co2 = c["0"] if len(c["1"]) >= len(c["0"]) else c["1"]
+
+    return int(oxy[0], 2) * int(co2[0], 2)
 
 
 if __name__ == "__main__":
