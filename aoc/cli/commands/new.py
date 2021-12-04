8dec4629e81fcdef4e2b7f96f515c0ee9e02b8a5
Juan Gonzalez <jrg2156@gmail.com>
2021-12-03 22:36:37 -0500

    Add aoc new commmand

--- aoc/cli/commands/new.py
+++ aoc/cli/commands/new.py
@@ -1,6 +1,31 @@
 import click
+import pendulum
+from pathlib import Path
+import subprocess
+import aoc.new_script
 
 
 @click.command()
-def new():
-    pass
+@click.option(
+    "-y",
+    "--year",
+    type=str,
+    prompt=True,
+    default=lambda: str(pendulum.now(tz="US/Eastern").year),
+)
+@click.option(
+    "-d",
+    "--day",
+    type=str,
+    prompt=True,
+    default=lambda: str(pendulum.now(tz="US/Eastern").day),
+)
+def new(year: str, day: str):
+    script_file = aoc.new_script(year=year, day=day)
+
+    print(f"Created script {script_file}!")
+
+    subprocess.Popen(
+        f"$EDITOR {script_file}",
+        shell=True,
+    )
