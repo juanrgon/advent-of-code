65f4f548cd4b4b190fa64f949000814ecf0941dd
Juan Gonzalez <jrg2156@gmail.com>
2021-12-03 21:23:40 -0500

    Add aoc cli code

---
+++ aoc/cli/cli.py
@@ -0,0 +1,30 @@
+import click
+import os
+from pathlib import Path
+import importlib
+
+
+class AocCommand(click.MultiCommand):
+    """
+    Click command with lazy loading subcommands.
+
+    Expects all commands to be in the 'commands' directory with a function with the same
+    name.
+    """
+
+    def list_commands(self, ctx):
+        """Return the names of all the files in the commands dir."""
+        return {
+            p.stem: self.get_command(None, p.stem)
+            for p in (Path(__file__).parent / "commands").iterdir()
+            if p.suffix == '.py' and not p.name.startswith('_')
+        }
+
+    def get_command(self, ctx, name):
+        """Return the names of all the files in the commands dir."""
+        return getattr(importlib.import_module(f"aoc.cli.commands.{name}"), name)
+
+
+@click.command(cls=AocCommand)
+def cli():
+    pass
---
+++ aoc/cli/commands/submit.py
@@ -0,0 +1,34 @@
+import click
+import importlib.util
+from typing import List
+from pathlib import Path
+import aoc
+
+
+@click.command()
+@click.argument("local_scriptpath", metavar="SCRIPT")
+@click.option(
+    "--part",
+    "parts",
+    default=["1", "2"],
+    type=click.Choice(["1", "2"]),
+    multiple=True,
+    help="A specific part of the puzzle to submit, either 1 or 2. Defaults to submitting both if omitted.",
+)
+def submit(local_scriptpath: str, parts: List[int]):
+    """Submit solution(s) from script to AOC."""
+
+    script = Path(local_scriptpath).absolute()
+
+    # load script. process grabbed from docs in importlib module
+    spec = importlib.util.spec_from_file_location(script.stem, str(script))
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    for i in parts:
+        part = getattr(module, f'part_{i}')
+
+        puzzle = aoc.get_puzzle(script)
+        solution = part(puzzle)
+
+        aoc.submit_puzzle(script_filename=script, part=i, solution=solution)
