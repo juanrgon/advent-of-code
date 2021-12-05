3388608dfdcf889be09d2f169c50d55dc730d1f1
Juan Gonzalez <jrg2156@gmail.com>
2021-12-05 13:04:44 -0500

    Allow override of scripts in the future

--- aoc/new_script.py
+++ aoc/new_script.py
@@ -1,14 +1,23 @@
 from pathlib import Path
+import pendulum
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def new_script(year: str, day: str, overwrite: bool=False) -> Path:
+def new_script(year: str, day: str, overwrite: bool = False) -> Path:
     day = day.zfill(2)
 
     script_dir = Path(__file__).parent.parent / year / day
 
-    if script_dir.exists() and not overwrite:
-        raise RuntimeError(f"Script already exists for {year}-{day}!!!")
-
+    if (
+        script_dir.exists()
+        and not overwrite
+    ):
+        if pendulum.now() > pendulum.datetime(year=int(year), month=12, day=int(day)):
+            logger.info("Allow override of solution file, because in the future")
+        else:
+            raise RuntimeError(f"Script already exists for {year}-{day}!!!")
 
     script_dir.mkdir(parents=True, exist_ok=True)
 
@@ -19,7 +28,6 @@ def new_script(year: str, day: str, overwrite: bool=False) -> Path:
 
     script.touch(exist_ok=True)
     puzzle.touch(exist_ok=True)
-
 
     script.write_text(
         (Path(__file__).parent / "templates" / "script" / filename).read_text()
