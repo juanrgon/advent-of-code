bdc650acd402ee9fecc27fd932043e3005e1e1cf
Juan Gonzalez <jrg2156@gmail.com>
2021-12-08 02:02:13 -0500

    cleanup

--- 2021/08/main.py
+++ 2021/08/main.py
@@ -34,40 +34,6 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 from functools import singledispatch
 import aoc
 
-"""
-  0:      1:      2:      3:      4:
- aaaa    ....    aaaa    aaaa    ....
-b    c  .    c  .    c  .    c  b    c
-b    c  .    c  .    c  .    c  b    c
- ....    ....    dddd    dddd    dddd
-e    f  .    f  e    .  .    f  .    f
-e    f  .    f  e    .  .    f  .    f
- gggg    ....    gggg    gggg    ....
-
-  5:      6:      7:      8:      9:
- aaaa    aaaa    aaaa    aaaa    aaaa
-b    .  b    .  .    c  b    c  b    c
-b    .  b    .  .    c  b    c  b    c
- dddd    dddd    ....    dddd    dddd
-.    f  e    f  .    f  e    f  .    f
-.    f  e    f  .    f  e    f  .    f
- gggg    gggg    ....    gggg    gggg
-"""
-
-BOARD = {
-    frozenset({"a", "b", "c", "e", "f", "g"}): 0,
-    frozenset({"c", "f"}): 1,
-    frozenset({"a", "c", "d", "e", "g"}): 2,
-    frozenset({"a", "c", "d", "f", "g"}): 3,
-    frozenset({"b", "c", "d", "f"}): 4,
-    frozenset({"a", "b", "d", "f", "g"}): 5,
-    frozenset({"a", "b", "d", "e", "f", "g"}): 6,
-    frozenset({"a", "c", "f"}): 7,
-    frozenset({"a", "b", "c", "d", "e", "f", "g"}): 8,
-    frozenset({"a", "b", "c", "d", "f", "g"}): 9,
-}
-
-
 
 @aoc.tests([TEST])
 @aoc.parse_text
