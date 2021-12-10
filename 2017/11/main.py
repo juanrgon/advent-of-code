ee3fc4a3e66a71aa5c42d81f2f240e2f4568acba
Juan Gonzalez <jrg2156@gmail.com>
2021-12-10 12:25:04 -0500

    2017 Day 11

---
+++ 2017/11/main.py
@@ -0,0 +1,42 @@
+TEST = [
+(
+"""
+""",
+    -42,
+)
+]
+
+TEST2 = [
+(
+"""
+""",
+-42,
+)
+]
+
+
+import aoc
+
+
+@aoc.submit
+@aoc.tests(TEST)
+@aoc.parse_text
+def part_1(raw: str, ints: list[int], strs: list[str]):
+    return 0
+
+
+@aoc.submit
+@aoc.tests(TEST2)
+@aoc.parse_text
+def part_2(raw: str, ints: list[int], strs: list[str]):
+    return 0
+
+
+if __name__ == "__main__":
+    puzzle = aoc.get_puzzle(__file__)
+
+    part_1.test()
+    print("Part 1:", part_1(puzzle))
+
+    # part_2.test()
+    # print("Part 2:", part_2(puzzle))
