fe34c66f7fb6e033d5c278cb8fef1a7ae0638f20
Juan Gonzalez <jrg2156@gmail.com>
2021-12-10 13:34:57 -0500

    Update aoc new

--- aoc/cli/commands/new.py
+++ aoc/cli/commands/new.py
@@ -6,62 +6,27 @@ import os
 import logging
 from pathlib import Path
 
-logger = logging.getLogger(__file__)
-
-
-def _default_date() -> pendulum.DateTime:
-    """
-    Default date for new AOC script.
-
-    If it is near midnight in December, use the "current" AOC puzzle as the default.
-
-    Else, we use today's date.
-
-    Allow AOC_YEAR env to override the default date.
-    """
-    east = "US/Eastern"
-
-    now = pendulum.now(tz=east)
-
-    if now.month == 12:
-        if now.hour == 23:
-            # if it's right before 12AM in December, use tomorrow as the default date
-            # because it's almost AOC time
-            return pendulum.tomorrow(east)
-        elif now.hour == 0:
-            # if it's after 12AM in December, use yestrday as the default date because
-            # you probably want to do yesteray's date
-            return pendulum.today(east)
-
-    year = int(os.environ.get("AOC_YEAR",0)) or now.year
-
-    year_dir = Path(__file__).parent.parent.parent.parent / str(year)
-
-    if not year_dir.exists():
-        day = 1
-    else:
-        day = max([int(p.stem) for p in year_dir.iterdir()]) + 1
-
-    return pendulum.datetime(int(year), 12, day, 0, 0, 0)
-
 
 @click.command()
 @click.option(
     "-y",
     "--year",
-    type=str,
-    prompt=True,
-    default=lambda: str(_default_date().year),
+    type=int,
 )
 @click.option(
     "-d",
     "--day",
-    type=str,
-    prompt=True,
-    default=lambda: str(_default_date().day),
+    type=int,
 )
-def new(year: str, day: str):
+def new(year: int, day: int):
     """Create new script for AOC"""
+
+    
+    year = click.prompt(f"Year", default=_get_year())
+    day = click.prompt(f"Day", default=_get_day(year))
+
+    click.prompt("Please enter a valid integer", type=int)
+
     script_file = aoc.new_script(year=year, day=day)
 
     print(f"Created script {script_file}!")
@@ -71,3 +36,30 @@ def new(year: str, day: str):
             f"$EDITOR {script_file}",
             shell=True,
         )
+
+
+def _get_year() -> int:
+    east = "US/Eastern"
+
+    now = pendulum.now(tz=east)
+
+    if now.month == 12:
+        if now.hour == 23:
+            # if it's right before 12AM in December, use tomorrow as the default date
+            # because it's almost AOC time
+            return pendulum.tomorrow(east).year
+        elif now.hour == 0:
+            # if it's after 12AM in December, use yestrday as the default date because
+            # you probably want to do yesteray's date
+            return pendulum.today(east).year
+
+    return int(os.environ.get("AOC_YEAR", 0)) or now.year
+
+
+def _get_day(year: int) -> int:
+    year_dir = Path(__file__).parent.parent.parent.parent / str(year)
+
+    if not year_dir.exists():
+        return 1
+    else:
+        return max([int(p.stem) for p in year_dir.iterdir()]) + 1
