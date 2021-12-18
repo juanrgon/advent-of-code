501ce2ab65c24b81f3e8e25de2c1efc5dbeea9fd
Juan Gonzalez <jrg2156@gmail.com>
2021-12-17 20:45:50 -0500

    wip

--- 2017/11/main.py
+++ 2017/11/main.py
@@ -1,46 +1,44 @@
-TEST = [
-(
-"""
-""",
-    -42,
-)
-]
-
-TEST2 = [
-(
-"""
-""",
--42,
-)
-]
-
-
 import aoc
 
 
-@aoc.submit
+TESTS_1 = [
+    ("ne,ne,ne", 3),
+    ("ne,ne,sw,sw", 0),
+    ("ne,ne,s,s", 2),
+    ("se,sw,se,sw,sw", 3),
+]
+
+TESTS_2 = [
+    ("", -42),
+]
+
+import attr
+
+
+
 @aoc.submit(part=1)
 @aoc.get_input
-@aoc.tests(TEST)
+@aoc.tests(TESTS_1)
 @aoc.parse_text
 def part_1(raw: str, ints: list[int], strs: list[str]):
+    # build dict of cycle of 7
+    # cycle_of_7
+
+    # build dict of cycle of 2
+    # cycle_of_2
+
+
     return 0
 
 
-@aoc.submit
 @aoc.submit(part=2)
 @aoc.get_input
-@aoc.tests(TEST2)
+@aoc.tests(TESTS_2)
 @aoc.parse_text
 def part_2(raw: str, ints: list[int], strs: list[str]):
     return 0
 
 
 if __name__ == "__main__":
-    puzzle = aoc.get_puzzle(__file__)
-
-    part_1.test()
-    print("Part 1:", part_1(puzzle))
-
-    # part_2.test()
-    # print("Part 2:", part_2(puzzle))
+    print("Part 1:", part_1(__file__))
+    print("Part 2:", part_2(__file__))
