fc2124bc99a12b3f957cedb0b078ce9b073ef3ab
Juan Gonzalez <juanrgon@github.com>
2022-12-02 02:09:16 -0500

    fix

--- aoc/solution.py
+++ aoc/solution.py
@@ -2,10 +2,12 @@ from .parse_text import parse_text
 from .get_input import get_input
 from .tests import tests as run_tests
 from .submit import submit as submit_to_aoc
+import functools
 
 
 def solution(part: int, tests=[], submit=False):
     def decorator(fn):
+        @functools.wraps(fn)
         def fn_override(*args, **kwargs):
             f = parse_text(fn)
             f = run_tests(tests)(f)
