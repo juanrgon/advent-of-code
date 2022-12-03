13a9e3ff0f4fbed9980e03db79e43bed6cfbd029
Juan Gonzalez <juanrgon@github.com>
2022-12-03 00:22:48 -0500

    fix

--- aoc/solution.py
+++ aoc/solution.py
@@ -5,16 +5,14 @@ from .submit import submit as submit_to_aoc
 import functools
 
 
-def solution(part: int, tests=[], submit=False):
+def solution(part: int, tests=[], submit=True):
     def decorator(fn):
         @functools.wraps(fn)
         def fn_override(*args, **kwargs):
             f = parse_text(fn)
             f = run_tests(tests)(f)
             f = get_input(f)
-
-            if submit:
-                f = submit_to_aoc(part=part)(f)
+            f = submit_to_aoc(part=part)(f)
 
             return f(*args, **kwargs)
 
